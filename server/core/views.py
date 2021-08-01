e814821460d6a1bd3e662597718ff8a71d39c0df
Jonathan Kohen <jonathan.a.kohen@gmail.com>
2021-07-31 23:34:05 -0400

    added djangorestframework

--- server/core/views.py
+++ server/core/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from .products import products
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 
+@api_view(["GET"])
 def getRoutes(request):
     routes = [
         "/api/products",
@@ -13,4 +18,19 @@ def getRoutes(request):
         "/api/products/delete/<id>",
         "/api/products/<update>/<id>",
     ]
-    return JsonResponse(routes, safe=False)
+    return Response(routes)
+
+
+@api_view(["GET"])
+def getProducts(request):
+    return Response(products)
+
+
+@api_view(["GET"])
+def getProduct(request, pk):
+    # product = None
+    for i in products:
+        if i["_id"] == pk:
+            product = i
+            break
+    return Response(product)
